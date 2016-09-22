704c1c62b5b9ee43eb3bb7deaf9144a9a3bf431f
Robbie Luijben <robbie.luijben@snappet.org>
2016-09-22 16:21:27 +0200

    Added basic navigation

--- src/PBEye/PBEye/App.xaml.cs
+++ src/PBEye/PBEye/App.xaml.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace PBEye
 {
-	public partial class App : Application
+	public partial class App
 	{
 		public App()
 		{
 			InitializeComponent();
 
-			MainPage = new PBEye.MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart()
--- src/PBEye/PBEye/MainPage.xaml.cs
+++ src/PBEye/PBEye/MainPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace PBEye
+﻿using System;
+
+namespace PBEye
 {
 	public partial class MainPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		private async void Login(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new SprintList());
 		}
 	}
 }
