11351814aa8ab9fba637c79e70bd274fa237fa23
Robbie Luijben <robbie.luijben@snappet.org>
2016-09-22 17:07:09 +0200

    Custom list items

---
+++ src/PBEye/PBEye/Models/WorkItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBEye.Models
+{
+	public class WorkItem
+	{
+		public string Title { get; set; }
+		public int WorkItemNumber { get; set; }
+	}
+}
--- src/PBEye/PBEye/Views/WorkItemList.xaml.cs
+++ src/PBEye/PBEye/Views/WorkItemList.xaml.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PBEye.Models;
 using Xamarin.Forms;
 
 namespace PBEye.Views
 {
 	public partial class WorkItemList : ContentPage
 	{
+		public ObservableCollection<WorkItem> WorkItems { get; set; }
+
 		public WorkItemList()
 		{
+			WorkItems = new ObservableCollection<WorkItem>
+			{
+				new WorkItem { Title = "Implement crap", WorkItemNumber = 12903 },
+				new WorkItem { Title = "Design crap", WorkItemNumber = 11730 },
+				new WorkItem { Title = "R&D crap", WorkItemNumber = 13066 },
+				new WorkItem { Title = "More crap", WorkItemNumber = 12954 },
+				new WorkItem { Title = "Revert previous crap", WorkItemNumber = 13085 },
+				new WorkItem { Title = "Discuss crap", WorkItemNumber = 12952 }
+			};
+
 			InitializeComponent();
 		}
 	}
