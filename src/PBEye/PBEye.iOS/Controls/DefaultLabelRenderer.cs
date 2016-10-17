71d7d421923d0dfea320fbd756e1f26499acf485
Robbie Luijben <robbie.luijben@snappet.org>
2016-10-17 11:18:01 +0200

    null check in renderer

--- src/PBEye/PBEye.iOS/Controls/DefaultLabelRenderer.cs
+++ src/PBEye/PBEye.iOS/Controls/DefaultLabelRenderer.cs
@@ -14,37 +14,39 @@ namespace PBEye.iOS.Controls
 		{
 			base.OnElementChanged(e);
 
-			if (Element.FontFamily == null)
+			if (Element != null)
 			{
-				Control.Font = UIFont.FromName(Element.FontAttributes == FontAttributes.Bold ?
-											   "AvenirNextLTPro-Bold" : "AvenirNextLTPro-Regular",
-											   Control.Font.PointSize);
-			}
-
-	
-
-			if (Element.Text != null)
-			{
-				float lineSpacing = 5;
-
-				if (Math.Abs(lineSpacing - (-1)) > 0.05)
+				if (Element.FontFamily == null)
 				{
-					var paragraphStyle = new NSMutableParagraphStyle
+					Control.Font = UIFont.FromName(Element.FontAttributes == FontAttributes.Bold ?
+												   "AvenirNextLTPro-Bold" : "AvenirNextLTPro-Regular",
+												   Control.Font.PointSize);
+				}
+
+				if (Element.Text != null)
+				{
+					float lineSpacing = 5;
+
+					if (Math.Abs(lineSpacing - (-1)) > 0.05)
 					{
-						LineSpacing = lineSpacing,
-						LineBreakMode = (UILineBreakMode)Enum.Parse(typeof(UILineBreakMode), Element.LineBreakMode.ToString())
-					};
+						var paragraphStyle = new NSMutableParagraphStyle
+						{
+							LineSpacing = lineSpacing,
+							LineBreakMode = (UILineBreakMode)Enum.Parse(typeof(UILineBreakMode), Element.LineBreakMode.ToString())
+						};
 
 
 
-					var text = new NSMutableAttributedString(Element.Text);
-					var style = UIStringAttributeKey.ParagraphStyle;
-					var range = new NSRange(0, text.Length);
+						var text = new NSMutableAttributedString(Element.Text);
+						var style = UIStringAttributeKey.ParagraphStyle;
+						var range = new NSRange(0, text.Length);
 
-					text.AddAttribute(style, paragraphStyle, range);
-					Control.AttributedText = text;
+						text.AddAttribute(style, paragraphStyle, range);
+						Control.AttributedText = text;
+					}
 				}
 			}
+
 		}
 	}
 }
